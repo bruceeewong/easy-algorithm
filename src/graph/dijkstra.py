273ff24103dac208b8370bfd3d96b6fcb2a3680e
bruski <292803281@qq.com>
2022-01-27 09:54:57 +0800

    feat: enhance dijkstra algorithm to support return path and lowest_cost

--- src/graph/dijkstra.py
+++ src/graph/dijkstra.py
@@ -1,30 +1,74 @@
 import unittest
 
+# def dijkstra(graph, costs, parents):
+#     processed = []
+#
+#     def find_lowest_cost_node(costs):
+#         lowest_cost = float('inf')
+#         lowest_cost_node = None
+#         for node in costs:
+#             cost = costs[node]
+#             if cost < lowest_cost and node not in processed:
+#                 lowest_cost = cost
+#                 lowest_cost_node = node
+#         return lowest_cost_node
+#
+#     node = find_lowest_cost_node(costs)
+#     while node is not None:
+#         cost = costs[node]
+#         neighbors = graph[node]
+#         for n in neighbors.keys():
+#             new_cost = cost + neighbors[n]
+#             if new_cost < costs[n]:
+#                 costs[n] = new_cost
+#                 parents[n] = node
+#         processed.append(node)
+#         node = find_lowest_cost_node(costs)
 
-def dijkstra(graph, costs, parents):
-    processed = []
+inf = float('inf')
 
-    def find_lowest_cost_node(costs):
-        lowest_cost = float('inf')
+
+def dijkstra(graph: dict, costs: dict, parents: dict, settings: dict):
+    def find_lowest_cost_node(costs: dict, processed: list):
+        lowest_cost = inf
         lowest_cost_node = None
-        for node in costs:
-            cost = costs[node]
-            if cost < lowest_cost and node not in processed:
-                lowest_cost = cost
+        for node in costs.keys():
+            if node in processed:
+                continue
+            node_cost = costs[node]
+            if node_cost < lowest_cost:
+                lowest_cost = node_cost
                 lowest_cost_node = node
         return lowest_cost_node
 
-    node = find_lowest_cost_node(costs)
+    def get_lowest_cost_path(parents: dict, fin_node):
+        result = []
+        next = fin_node
+        while next is not None:
+            result.append(next)
+            next = parents[next] if next in parents else None
+        result.reverse()
+        return result
+
+    processed = []
+    node = find_lowest_cost_node(costs, processed)
     while node is not None:
-        cost = costs[node]
         neighbors = graph[node]
+        cost = costs[node]
         for n in neighbors.keys():
             new_cost = cost + neighbors[n]
-            if costs[n] > new_cost:
+            if new_cost < costs[n]:
                 costs[n] = new_cost
                 parents[n] = node
         processed.append(node)
-        node = find_lowest_cost_node(costs)
+        node = find_lowest_cost_node(costs, processed)
+
+    fin_node = settings.get('fin')
+    result = {
+        'path': get_lowest_cost_path(parents, fin_node),
+        'lowest_cost': costs[fin_node]
+    }
+    return result
 
 
 class TestDijkstra(unittest.TestCase):
@@ -37,7 +81,6 @@ class TestDijkstra(unittest.TestCase):
         graph["b"]["fin"] = 5
         graph["fin"] = {}
 
-        inf = float('inf')
         costs = {}
         costs['a'] = 6
         costs['b'] = 2
@@ -48,12 +91,17 @@ class TestDijkstra(unittest.TestCase):
         parents['b'] = 'start'
         parents['fin'] = None
 
-        result = {
-            "path": ['start', 'b', 'a', 'fin'],
-            "lowest_cost": 5
+        settings = {
+            'fin': 'fin'
         }
 
-        dijkstra(graph, costs, parents)
+        expectation = {
+            "path": ['start', 'b', 'a', 'fin'],
+            "lowest_cost": 6
+        }
+
+        result = dijkstra(graph, costs, parents, settings)
         self.assertEqual(6, costs['fin'])
         self.assertEqual(2, costs['b'])
         self.assertEqual(5, costs['a'])
+        self.assertEqual(expectation, result)
